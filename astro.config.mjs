06e8da3a71124ba72c735b7169ba464708513c4b
edydiazma <56948901+edydiazma@users.noreply.github.com>
2025-02-27 00:27:21 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
-// Import the Vercel adapter
-import vercel from '@astrojs/vercel';
+import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://positivustheme.vercel.app",
+  site: "https://brandandbyte.pages.dev",
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
+  output: 'static', 
+  adapter: cloudflare(),
 });
