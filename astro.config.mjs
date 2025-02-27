f55d3433db6564b13cb159690d551e83b9eea0bd
edydiazma <56948901+edydiazma@users.noreply.github.com>
2025-02-27 00:56:51 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
+import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  site: "https://brandandbyte.pages.dev",
+  site: 'https://brandandbyte.pages.dev',
   integrations: [tailwind()],
-  output: 'static', 
+  output: 'static',
   adapter: cloudflare(),
 });
